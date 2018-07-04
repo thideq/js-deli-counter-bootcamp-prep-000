fb5fd991fa53d7a13d5474d04eee3289add3dcbc
Thierry Dequin <thierrydequin@hotmail.com>
2018-07-04 15:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var myName = 'my name';
 function takeANumber(katzDeliLine,myName) {
   var myString = '';
   katzDeliLine.push(myName)
+  if (katzDeliLine.length=1) {
+    myString = "you are" + (katzDeliLine.length) + "st in line";
+  }
   myString = "you are" + (katzDeliLine.length) + "th in line";
   return myString;
 }
