6638e715deada14bcc610001acc048ab9fc65733
Thierry Dequin <thierrydequin@hotmail.com>
2018-07-04 15:15:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function takeANumber(katzDeliLine,myName) {
   var myString = '';
   katzDeliLine.push(myName)
   if (katzDeliLine.length==1) {
-    myString = "you are" + (katzDeliLine.length) + "st in line";
+    myString = "you are " + (katzDeliLine.length) + "st in line";
   }
     else if (katzDeliLine.length==2) {
-    myString = "you are" + (katzDeliLine.length) + "nd in line";
+    myString = "you are " + (katzDeliLine.length) + "nd in line";
   }
     else if (katzDeliLine.length==3) {
-    myString = "you are" + (katzDeliLine.length) + "rd in line";
+    myString = "you are " + (katzDeliLine.length) + "rd in line";
   }
   else {myString = "you are " + (katzDeliLine.length) + "th in line";}
   return myString;
 }
-console.log(takeANumber(["1", "2"],"moi"));
+console.log(takeANumber(["1", "2", "3"],"moi"));
